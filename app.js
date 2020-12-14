3973b7ceac38543844609f4f1631572e5e6e466e
YorKeN <dethoor.axel@gmail.com>
2020-12-14 23:37:49 +0100

    add user route / modele / controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const produitRoutes = require('./routes/produit');
+const userRoutes = require('./routes/user');
 const path = require('path');
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.92r9j.mongodb.net/produits?retryWrites=true&w=majority', {
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/produit', produitRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- modeles/user.js
+++ modeles/user.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const userShema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    mdp: { type: String, required: true },
     prenom: { type: String, required: true },
     nom: { type: String, required: true },
     adresse: { type: String, required: true },
-    codePostal: { type: String, required: true },
+    codePostal: { type: Number, required: true },
     ville: { type: String, required: true },
 });
 
