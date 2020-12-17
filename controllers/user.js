695112b31387d3541524fb9e117f2122e9bb2526
YorKeN <dethoor.axel@gmail.com>
2020-12-17 20:40:58 +0100

    add getUserById

--- controllers/user.js
+++ controllers/user.js
@@ -43,3 +43,19 @@ exports.login = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error }));
 };
+
+exports.getUserById = (req, res, next) => {
+    User.findOne({
+        _id: req.params.id
+    }).then(
+        (user) => {
+            res.status(200).json(user);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    )
+}
